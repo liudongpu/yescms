c0c9c2849dc271af7269c97cfdd0a4a2faf82b67
srnpr <srnpr@qh-liudp>
2015-09-30 22:19:58 +0800

    a

--- yescms/src/main/java/com/srnpr/yescms/helper/YesCmsHelper.java
+++ yescms/src/main/java/com/srnpr/yescms/helper/YesCmsHelper.java
@@ -1,5 +1,8 @@
 package com.srnpr.yescms.helper;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.apache.commons.lang.StringUtils;
 
 import com.srnpr.zapcom.basehelper.RegexHelper;
@@ -9,19 +12,14 @@ public class YesCmsHelper {
 	public static String formatHtml(String sString)
 
 	{
-
-		return sString.replaceAll("<html.*</html>", "");
+		return StringUtils.replace(
+				StringUtils.replace(sString, StringUtils.substringBetween(sString, "<html", "</html>"), ">"),
+				"<html></html>", "");
 
 	}
-	
-	
-	
-	
-	public static String formatLink(String sString)
-	{
+
+	public static String formatLink(String sString) {
 		return StringUtils.replace(sString, "/ycsite/", "/ycwap/");
 	}
-	
-	
 
 }
