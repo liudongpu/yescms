d2787408cf52cb8c7cacd991eabf100fda264883
srnpr <srnpr@qh-liudp>
2016-03-24 19:59:57 +0800

    a

--- yescms/src/test/java/com/srnpr/yescms/AppTest.java
+++ yescms/src/test/java/com/srnpr/yescms/AppTest.java
@@ -1,13 +1,13 @@
 package com.srnpr.yescms;
 
-import org.junit.Test;
 
-import com.srnpr.zapcom.basehelper.TestHelper;
+
+
 
 /**
  * Unit test for simple App.
  */
-public class AppTest extends TestHelper
+public class AppTest 
 
 {
 
